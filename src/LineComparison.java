99ce0b5a19c7df636a16112d90444acd783ae493
yashdchintawar <yashchintawar31@gmail.com>
2022-03-08 08:53:17 +0530

    Welcome message

---
+++ src/LineComparison.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Line Compariosn Problem");
+    }
+}
