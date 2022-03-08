0fff5a468d0eafb50b8ae21c7bee8a1574848b7c
yashdchintawar <yashchintawar31@gmail.com>
2022-03-08 09:26:55 +0530

    uc02 checking line comparison

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -7,13 +7,32 @@ public class LineComparison {
         Scanner in = new Scanner(System.in);
 
         System.out.println("Line 01 :- enter X1 and Y1 value"); //enter value
-        int x1 = in.nextInt();
-        int y1 = in.nextInt();
-
+        int line1X1 = in.nextInt();
+        int line1Y1 = in.nextInt();
         System.out.println("Line 01 :- enter X2 and Y2 value");
-        int x2 = in.nextInt();
-        int y2 = in.nextInt();
+        int line1X2 = in.nextInt();
+        int line1Y2 = in.nextInt();
 
-        double line1 = Math.sqrt( (x2 - x1) ^ 2 + (y2 - y1) ^ 2 );
+        int line1 =(int) Math.sqrt( (line1X2 - line1X1) ^ 2 + (line1Y2 - line1Y1) ^ 2 );
+        System.out.println("Line One Length Is:- " + line1);
+
+
+        System.out.println("Line 02 :- enter X1 and Y1 value"); //enter value
+        int line2X1 = in.nextInt();
+        int line2Y1 = in.nextInt();
+        System.out.println("Line 02 :- enter X2 and Y2 value");
+        int line2X2 = in.nextInt();
+        int line2Y2 = in.nextInt();
+
+        int line2 = (int)Math.sqrt( (line2X2 - line2X1) ^ 2 + (line2Y2 - line2Y1) ^ 2 );
+        System.out.println("Line One Length Is:- " + line1);
+
+
+        if (line1 == line2){
+            System.out.println("Line 01 And Line 02 Is Equal");
+        }
+        else {
+            System.out.println("Line 01 And Line 02 Is Not Equal");
+        }
+        }
     }
-}
