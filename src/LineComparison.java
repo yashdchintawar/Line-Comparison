a73665ffe43fd459e033de7b4d2c623dc70b3813
yashdchintawar <yashchintawar31@gmail.com>
2022-03-08 08:57:19 +0530

    Uc2 line comparison calculating line length

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Line Compariosn Problem");
+        System.out.println("Welcome to Line Comparison Computation Program");
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Line 01 :- enter X1 and Y1 value");
+        int x1 = in.nextInt();
+        int y1 = in.nextInt();
+
+        System.out.println("Line 01 :- enter X2 and Y2 value");
+        int x2 = in.nextInt();
+        int y2 = in.nextInt();
+
+        double line1 = Math.sqrt( (x2 - x1) ^ 2 + (y2 - y1) ^ 2 );
     }
 }
